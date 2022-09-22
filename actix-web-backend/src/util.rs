2d44d8b7ab424187c3ae8219b39252f04305ac20
Kevin Geronimo <96751432+kevingeronimo@users.noreply.github.com>
2022-09-22 23:29:40 +0000

    Replace bcrypt with argon2

--- actix-web-backend/src/util.rs
+++ actix-web-backend/src/util.rs
@@ -1,23 +1,28 @@
-use bcrypt::DEFAULT_COST;
+use actix_web::rt::task;
+use argon2::{
+    password_hash::{rand_core::OsRng, PasswordHash, PasswordHasher, PasswordVerifier, SaltString},
+    Argon2,
+};
 
 pub async fn hash_password(password: String) -> anyhow::Result<String> {
-    let (send, recv) = tokio::sync::oneshot::channel();
-    rayon::spawn(move || {
-        let result = bcrypt::hash(password, DEFAULT_COST);
-        let _ = send.send(result);
-    });
-    recv.await
-        .map_err(|e| e.into())
-        .and_then(|result| result.map_err(|e| e.into()))
+    let salt = SaltString::generate(&mut OsRng);
+    let argon2 = Argon2::default();
+
+    task::spawn_blocking(move || {
+        argon2
+            .hash_password(password.as_bytes(), &salt)
+            .map(|v| v.to_string())
+            .map_err(|e| anyhow::anyhow!(e))
+    })
+    .await?
 }
 
 pub async fn verify_password(password: String, hash: String) -> anyhow::Result<bool> {
-    let (send, recv) = tokio::sync::oneshot::channel();
-    rayon::spawn(move || {
-        let result = bcrypt::verify(password, &hash);
-        let _ = send.send(result);
-    });
-    recv.await
-        .map_err(|e| e.into())
-        .and_then(|result| result.map_err(|e| e.into()))
+    task::spawn_blocking(move || {
+        let parsed_hash = PasswordHash::new(&hash).map_err(|e| anyhow::anyhow!(e))?;
+        Ok(Argon2::default()
+            .verify_password(password.as_bytes(), &parsed_hash)
+            .is_ok())
+    })
+    .await?
 }
